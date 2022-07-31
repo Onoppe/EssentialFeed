a32a1939b6b251868676db3edba3a6095aa33a77
Omar Noppe <info@ownmac.com>
2022-07-31 20:23:01 +0200

    first commit

--- EssentialFeed/EssentialFeed.h
+++ EssentialFeed/EssentialFeed.h
@@ -13,6 +13,4 @@ FOUNDATION_EXPORT double EssentialFeedVersionNumber;
 //! Project version string for EssentialFeed.
 FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>                  
--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -6,30 +6,67 @@
 //
 
 import XCTest
-@testable import EssentialFeed
+import EssentialFeed
 
-class EssentialFeedTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        var capteredErrors = [RemoteFeedLoader.Error?]()
+        sut.load { capteredErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code:     0)
+        
+        client.complete(with: clientError)
+        
+        XCTAssertEqual(capteredErrors, [.connectivity])
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+         var completions = [(Error) -> Void]()
+        
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+                completions.append(completion)
+            requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 
