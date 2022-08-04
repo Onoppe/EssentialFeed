7de81f4dd0841bfd956133f0b1d4fce3d772fa0b
Omar Noppe <info@ownmac.com>
2022-08-04 16:12:49 +0200

    Simplify type declarations with a helper function

--- EssentialFeedTests/Feed Api/EssentialFeedTests.swift
+++ EssentialFeedTests/Feed Api/EssentialFeedTests.swift
@@ -20,7 +20,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         
         
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompleteWithResult: failure(RemoteFeedLoader.Error.connectivity)) {
             let clientError = NSError(domain: "Test", code:     0)
             client.complete(with: clientError)
         }
@@ -32,7 +32,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach { index, code in
             
-            expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWithResult: failure(RemoteFeedLoader.Error.invalidData)) {
                 let json = makeItemsJSON([])
                 
                 client.complete(withStatusCode: code, data: json, at: index)
@@ -63,7 +63,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWithResult: failure(.invalidData)) {
             let invalidJSON = Data("Invalid JSON".utf8)
             
             client.complete(withStatusCode: 200, data: invalidJSON)
@@ -126,6 +126,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         trackMemoryLeaks(client, file: file, line: line)
 
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
